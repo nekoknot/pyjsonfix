bbe02d7b33336225cb45401890cd742f5da898eb
Jessica <nekoknot@proton.me>
2012-12-19 13:53:58 -0500

    Added a simple setup.py and fixed up the example in the README

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="pyjsonfix",
+    version="0.0.1",
+    description="Does its best to turn Javascript object literals into valid JSON",
+    author="Jessica (nekoknot)",
+    author_email="nekokno@proton.me",
+    url="https://github.com/nekoknot/pyjsonfix",
+    py_modules=["jsonfix"],
+      )
